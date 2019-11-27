fd74051c5f205b6c57ca67f3415bb2e957f50a5a
Vertjs <elena.viktorovna.bogatyreva@gmail.com>
2019-11-27 14:35:24 +0300

    init

---
+++ frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function App() {
+    console.log('object')
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+App.propTypes = {
+
+}
+
+export default App
+
